ee5b2d021f48aed0d39fec71aefa5b19aa8a7d29
Domara Hawil <domara.hawil@hotmail.com>
2023-05-16 16:22:07 -0400

    added Admin Dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
- 
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
